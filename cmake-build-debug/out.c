ec2ec1714b98fa9043bf44ff3e66fd5a4faf7b56
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2022-06-14 14:39:37 -0400

    test

--- cmake-build-debug/out.c
+++ cmake-build-debug/out.c
@@ -2,11 +2,24 @@
 
 int main() {
 
-    int x;
-    float pi;
+    float nums;
+    int a;
+    int b;
+    int c;
 
-    x = 4;
-    pi = 3.14;
-    printf("%.2f\n", (float) (2 + 2 + pi + x));
+    printf("How man fibonacci numbers do you want?\n");
+    scanf("%f", &nums);
+    printf(" \n");
+    a = 0;
+    b = 1;
+    c = 0;
+    while (nums > 0) {
+        printf("%.2f\n", (float) (a));
+        c = a + b;
+        a = b;
+        b = c;
+        nums = nums - 1;
+
+    }
     return 0;
 }
