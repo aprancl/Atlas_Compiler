f3b409ae6656be307faf6c7f420d147843f1be0f
Anthony Prancl <aprancl@mocs.flsouthern.edu>
2022-11-09 11:43:39 -0500

    an update to either the LLVM or Clion has caused everything to break; i need to invesitgate further

--- io/out.c
+++ io/out.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 float factorial(float n) {
 //  returns n! factorial
-if (n == 0)
-{
-return 1;
+    if (n == 0) {
+        return 1;
 
-}
-else {
-float x = n - 1;
-float temp = factorial(x);
-return n * temp;
+    } else {
+        float x = n - 1;
+        float temp = factorial(x);
+        return n * temp;
 
+    }
+    return 1;
 }
-return 1;
-}
+
 int main() {
 
-float in;
+    float in;
 
 //  recursive factorial program in atlas
-printf("Please enter a number below\n");
-scanf("%f", &in);
-float answer = factorial(in);
-printf("%.2f\n", (float)(answer));
+    printf("Please enter a number below\n");
+    scanf("%f", &in);
+    float answer = factorial(in);
+    printf("%.2f\n", (float) (answer));
 
-return 0;
+    return 0;
 }
